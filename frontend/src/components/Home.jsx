d25000cc6dd7632613f043485a9bdfee8747e126
lakshanravi <rgrlranathunga@std.appsc.sab.ac.lk>
2025-06-18 22:07:28 +0530

    created latestjobs componet

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
+import Navbar from './shared/Navbar.jsx'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
