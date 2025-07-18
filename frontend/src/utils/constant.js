c71cbf38bb1b371796169e8b39ff83818e1b943c
lakshanravi <rgrlranathunga@std.appsc.sab.ac.lk>
2025-07-18 23:06:25 +0530

    changed the frontend constant file by  adding ec2 instance's ip address

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+
+//below one used to host in AWS EC2 instance 
+
+
+export const USER_API_END_POINT = "http://16.171.20.10/api/v1/user";
+export const JOB_API_END_POINT = "http://16.171.20.10/api/v1/job";
+export const APPLICATION_API_END_POINT = "http://16.171.20.10/api/v1/application";
+export const COMPANY_API_END_POINT = "http://16.171.20.10/api/v1/company";
